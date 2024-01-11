fe3e03b969fc97f5d57661a2e5df08ef2cf2e484
Manoj <127009204+manoj-6370@users.noreply.github.com>
2024-01-12 01:13:23 +0530

    Add files via upload

---
+++ src/main/java/Ram/manoj.java
@@ -0,0 +1,13 @@
+package Ram;
+
+public class manoj {
+
+	  public manoj() {
+
+	  }
+
+	  //TODO: Add javadoc comment
+	  public String greet(String someone) {
+	    return String.format("Hello, %s!", someone);
+	  }
+}
